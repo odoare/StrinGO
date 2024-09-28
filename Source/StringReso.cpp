91a27555149115dd031c8266efb000ab6b87c67e
Doaré Olivier <olivier.doare@ensta-paris.fr>
2024-09-28 21:42:21 +0200

    Increase LFO amplitudes for filter frequencies

--- Source/StringReso.cpp
+++ Source/StringReso.cpp
@@ -782,7 +782,7 @@ void StringReso::updateLfos()
     for (int l=0;l<NUMLFO;l++)
     {
         float lfomult = 1.f-(.5f+.5f*lfoVal[l])*params.lfoParams[l].amp;
-        float lfomult10 = 1.f-(.05f+.05f*lfoVal[l])*params.lfoParams[l].amp;
+        float lfomult10 = 1.f-(.2f+.2f*lfoVal[l])*params.lfoParams[l].amp;
 
         lfoFacSampleLevel *= params.lfoParams[l].samplerLevel ? lfomult : 1.f ;
         lfoFacSampleLPF *= params.lfoParams[l].samplerFreq ? lfomult : 1.f ;
